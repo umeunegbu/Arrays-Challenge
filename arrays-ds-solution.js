8f4b12e86315aed5c2ce021827dc383ade2be840
Umeunegbu Pascal <umeunegbupascal@gmail.com>
2024-10-20 12:54:54 +0100

    Add arrays-ds challenge and solution

---
+++ arrays-ds-solution.js
@@ -0,0 +1,11 @@
+function reverseArray(a){
+    let output = [];
+
+    for(let i=0; i<a.length; i++){
+        output.unshift(a[i]);
+    }
+    
+
+    return output;
+}
+module.exports = {reverseArray};
